b13ac7778f34ff9b25cc6754e3075b32aea42198
15153781 <15153781@LAB10-18.suicobrasileira.sp>
2016-08-11 16:15:31 -0300

    Commit inicial

---
+++ public_html/js/modal-automatica.js
@@ -0,0 +1,9 @@
+$(document).ready(function()
+{
+$("#meumodal").modal({
+                        backdrop: 'static',
+                        keyboard: true, 
+                        show: true
+                })
+});
+ 
